4004daa404ec5d528fe6d5ef0d9d60c224713b26
Richard Thomas <richardhthomas@yahoo.com>
2013-10-21 22:09:31 +0100

    email - PHPMailer included take 3

--- sendmail.php
+++ sendmail.php
@@ -4,7 +4,13 @@ require 'PHPMailer2/PHPMailerAutoload.php';
 
 $mail = new PHPMailer;
 
-$mail->isSendmail();
+//$mail->isSendmail();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 587;
+$mail->Host = 'smtp.sendgrid.net';
+$mail->Username = $_ENV["SENDGRID_USERNAME"];
+$mail->Password = $_ENV["SENDGRID_PASSWORD"];
 $mail->setFrom = $_POST['Emailaddr'];
 $mail->FromName = $_POST['Fullname'];
 $mail->addAddress('richard.thomas@itamus.com', 'itamus.com');
